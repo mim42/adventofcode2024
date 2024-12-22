9228dcbc2554ea36e0e46c4acd6d2b3a5bca74cb
Petros Minoudis <petros.minoudis@samedi.de>
2024-12-22 04:42:54 +0100

    restructure day 21

--- aoc21/src/main.rs
+++ aoc21/src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fs::read_to_string,
-    usize,
-};
+use std::{collections::HashMap, fs::read_to_string, usize};
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
@@ -12,10 +8,14 @@ fn read_lines(filename: &str) -> Vec<String> {
     result
 }
 
-fn next_buttons_keypad(keypad: &Vec<Vec<String>>, button: String) -> Vec<(String, String)> {
+fn next_buttons(
+    interface: &Vec<Vec<String>>,
+    button: String,
+    is_keypad: bool,
+) -> Vec<(String, String)> {
     let mut button_y = 0;
     let mut button_x = 0;
-    for (y, row) in keypad.iter().enumerate() {
+    for (y, row) in interface.iter().enumerate() {
         for (x, value) in row.iter().enumerate() {
             if button == *value {
                 button_y = y;
@@ -30,97 +30,32 @@ fn next_buttons_keypad(keypad: &Vec<Vec<String>>, button: String) -> Vec<(String
         let next_button_y = button_y as isize + op.0;
         let next_button_x = button_x as isize + op.1;
         if next_button_y >= 0
-            && next_button_y < keypad.len() as isize
+            && next_button_y < interface.len() as isize
             && next_button_x >= 0
-            && next_button_x < keypad[0].len() as isize
-            && !(next_button_y == 3 && next_button_x == 0)
+            && next_button_x < interface[0].len() as isize
         {
-            next_buttons.push((
-                keypad[next_button_y as usize][next_button_x as usize].clone(),
-                remote_instructions[index].to_string(),
-            ));
+            if is_keypad && (!(next_button_y == 3 && next_button_x == 0)) {
+                next_buttons.push((
+                    interface[next_button_y as usize][next_button_x as usize].clone(),
+                    remote_instructions[index].to_string(),
+                ));
+            } else if !is_keypad && !(next_button_y == 0 && next_button_x == 0) {
+                next_buttons.push((
+                    interface[next_button_y as usize][next_button_x as usize].clone(),
+                    remote_instructions[index].to_string(),
+                ));
+            }
         }
     }
 
     next_buttons
 }
 
-fn find_all_keypad_paths(
-    keypad: &Vec<Vec<String>>,
+fn find_all_paths(
+    interface: &Vec<Vec<String>>,
     button_a: String,
     button_b: String,
-) -> Vec<String> {
-    let mut q = Vec::new();
-    let mut processed: HashSet<String> = HashSet::new();
-    let mut distance: HashMap<String, usize> = HashMap::new();
-    distance.insert(button_a.clone(), 0);
-    q.push((button_a, 0, "".to_string()));
-    while !q.is_empty() {
-        q.sort_by(|a, b| a.1.cmp(&b.1));
-        q.reverse();
-        let (a, cost, path) = q.pop().unwrap();
-        if a == button_b {
-            q.push((a, cost, path));
-            break;
-        }
-        processed.insert(a.clone());
-        for (next_button, remote_instruction) in next_buttons_keypad(keypad, a) {
-            if !distance.contains_key(&next_button) {
-                distance.insert(next_button.clone(), cost + 1);
-                q.push((next_button, cost + 1, path.clone() + &remote_instruction));
-            } else {
-                let old_cost = distance.get_mut(&next_button).unwrap();
-                if cost + 1 <= *old_cost {
-                    *old_cost = cost + 1;
-                    q.push((next_button, cost + 1, path.clone() + &remote_instruction));
-                }
-            }
-        }
-    }
-
-    q.iter()
-        .filter(|(button, _, _)| *button == button_b)
-        .map(|(_, _, instruction)| instruction.clone() + "A")
-        .collect::<Vec<String>>()
-}
-
-fn next_buttons_remote(remote: &Vec<Vec<String>>, button: String) -> Vec<(String, String)> {
-    let mut button_y = 0;
-    let mut button_x = 0;
-    for (y, row) in remote.iter().enumerate() {
-        for (x, value) in row.iter().enumerate() {
-            if button == *value {
-                button_y = y;
-                button_x = x;
-            }
-        }
-    }
-
-    let mut next_buttons = Vec::new();
-    let ops = [(1, 0), (-1, 0), (0, 1), (0, -1)];
-    let remote_instructions = ["v", "^", ">", "<"];
-    for (index, op) in ops.iter().enumerate() {
-        let next_button_y = button_y as isize + op.0;
-        let next_button_x = button_x as isize + op.1;
-        if next_button_y >= 0
-            && next_button_y < remote.len() as isize
-            && next_button_x >= 0
-            && next_button_x < remote[0].len() as isize
-            && !(next_button_y == 0 && next_button_x == 0)
-        {
-            next_buttons.push((
-                remote[next_button_y as usize][next_button_x as usize].clone(),
-                remote_instructions[index].to_string(),
-            ));
-        }
-    }
-    next_buttons
-}
-
-fn find_all_remote_paths(
-    remote: &Vec<Vec<String>>,
-    button_a: String,
-    button_b: String,
+    is_keypad: bool,
 ) -> Vec<String> {
     if button_a == button_b {
         return vec!["A".to_string()];
@@ -137,7 +72,7 @@ fn find_all_remote_paths(
             q.push((a, cost, path));
             break;
         }
-        for (next_button, remote_instruction) in next_buttons_remote(remote, a) {
+        for (next_button, remote_instruction) in next_buttons(interface, a, is_keypad) {
             if !distance.contains_key(&next_button) {
                 distance.insert(next_button.clone(), cost + 1);
                 q.push((next_button, cost + 1, path.clone() + &remote_instruction));
@@ -162,7 +97,7 @@ fn remote_on_keypad(keypad: &Vec<Vec<String>>, code: String) -> Vec<String> {
     let mut pointin_button = "A".to_string();
     for button in code.chars() {
         let mut temp = Vec::new();
-        for path in find_all_keypad_paths(keypad, pointin_button.to_string(), button.to_string()) {
+        for path in find_all_paths(keypad, pointin_button.to_string(), button.to_string(), true) {
             for command in &remote_commands {
                 temp.push(command.clone() + &path);
             }
@@ -173,69 +108,55 @@ fn remote_on_keypad(keypad: &Vec<Vec<String>>, code: String) -> Vec<String> {
     remote_commands
 }
 
-// fn heuristic(a: &String) -> usize {
-//     let mut counter = 0;
-//     let mut prev_char = '_';
-//     for i in a.chars() {
-//         if i == prev_char {
-//             counter += 1;
-//         } else {
-//             prev_char = i;
-//         }
-//     }
-//     counter + 100 - a.len() * 3 as usize
-// }
-
 fn remote_on_remote(
     remote: &Vec<Vec<String>>,
     cache: &mut HashMap<(String, usize), usize>,
-    memory: &mut HashMap<String, Vec<String>>,
-    remote_counter: HashMap<String, usize>,
+    remote_code: &String,
     depth: usize,
 ) -> usize {
+    let mut remote_counter: HashMap<String, usize> = HashMap::new();
+    let split = remote_code
+        .split_inclusive("A")
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>();
+
+    for sub_codes in split {
+        remote_counter
+            .entry(sub_codes)
+            .and_modify(|c| *c += 1)
+            .or_insert(1);
+    }
     if depth == 0 {
         return remote_counter
             .iter()
             .map(|(code, counter)| code.len() * counter)
             .sum();
     }
-    let mut true_length = 0;
+    let mut total_length = 0;
     for (code, counter) in remote_counter {
-        let mut possible_codes = Vec::new();
         let mut min_length = usize::MAX;
         if !cache.contains_key(&(code.clone(), depth)) {
-            let mut remote_commands = vec!["".to_string()];
+            let mut possible_remote_commands = vec!["".to_string()];
             let mut pointin_button = "A".to_string();
 
             for button in code.chars() {
-                let mut temp = Vec::new();
-                for path in
-                    find_all_remote_paths(remote, pointin_button.to_string(), button.to_string())
-                {
-                    for command in &remote_commands {
-                        temp.push(command.clone() + &path);
+                let mut possible_remote_temp = Vec::new();
+                for path in find_all_paths(
+                    remote,
+                    pointin_button.to_string(),
+                    button.to_string(),
+                    false,
+                ) {
+                    for command in &possible_remote_commands {
+                        possible_remote_temp.push(command.clone() + &path);
                     }
                 }
-                remote_commands = temp;
+                possible_remote_commands = possible_remote_temp;
                 pointin_button = button.to_string();
             }
 
-            possible_codes = remote_commands.clone();
-
-            for possible_code in possible_codes {
-                let mut commands_counter: HashMap<String, usize> = HashMap::new();
-                let splited = possible_code
-                    .split_inclusive("A")
-                    .map(|c| c.to_string())
-                    .collect::<Vec<String>>();
-                for sub_codes in splited {
-                    commands_counter
-                        .entry(sub_codes)
-                        .and_modify(|c| *c += counter)
-                        .or_insert(counter);
-                }
-                let length =
-                    remote_on_remote(remote, cache, memory, commands_counter.clone(), depth - 1);
+            for possible_code in possible_remote_commands {
+                let length = remote_on_remote(remote, cache, &possible_code, depth - 1);
                 if length <= min_length {
                     min_length = length;
                 }
@@ -243,16 +164,14 @@ fn remote_on_remote(
             cache.insert((code.clone(), depth), min_length / counter);
         } else {
             min_length = *cache.get(&(code, depth)).unwrap() * counter;
-            println!("works")
         }
-
-        true_length += min_length
+        total_length += min_length
     }
 
-    true_length
+    total_length
 }
 
-fn solve_part_a(input: &Vec<String>) -> usize {
+fn find_length(input: &Vec<String>, robots: usize) -> usize {
     let keypad = vec![
         vec!["7", "8", "9"],
         vec!["4", "5", "6"],
@@ -268,29 +187,13 @@ fn solve_part_a(input: &Vec<String>) -> usize {
         .map(|row| row.iter().map(|c| c.to_string()).collect::<Vec<String>>())
         .collect::<Vec<Vec<String>>>();
 
+    let mut cache: HashMap<(String, usize), usize> = HashMap::new();
     let mut answer = 0;
     for code in input {
         let first_codes = remote_on_keypad(&keypad, code.clone());
-
         let final_code = first_codes
             .iter()
-            .map(|code| {
-                let mut cache: HashMap<(String, usize), usize> = HashMap::new();
-                let mut commands_counter: HashMap<String, usize> = HashMap::new();
-                let mut memory: HashMap<String, Vec<String>> = HashMap::new();
-                let splited = code
-                    .split_inclusive("A")
-                    .map(|c| c.to_string())
-                    .collect::<Vec<String>>();
-
-                for sub_codes in splited {
-                    commands_counter
-                        .entry(sub_codes)
-                        .and_modify(|c| *c += 1)
-                        .or_insert(1);
-                }
-                remote_on_remote(&remote, &mut cache, &mut memory, commands_counter, 25)
-            })
+            .map(|code| remote_on_remote(&remote, &mut cache, code, robots))
             .min_by(|a, b| a.cmp(&b))
             .unwrap();
 
@@ -304,8 +207,12 @@ fn solve_part_a(input: &Vec<String>) -> usize {
     answer
 }
 
+fn solve_part_a(input: &Vec<String>) -> usize {
+    find_length(input, 2)
+}
+
 fn solve_part_b(input: &Vec<String>) -> usize {
-    todo!()
+    find_length(input, 25)
 }
 
 fn main() {
